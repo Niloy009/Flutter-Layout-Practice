f6ad43d1641f11d37e9d3d97a6d023443e15b7c9
Niloy009 <niloy.roy@northsouth.edu>
2020-06-09 16:09:55 +0600

    banner bad disi

--- lib/screen/Layout.dart
+++ lib/screen/Layout.dart
@@ -5,6 +5,7 @@ class Layout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
